63f7699858ec586e7f2c5c8191f9cfa5e3ac7d28
CleverYeti <cleveryeti3@gmail.com>
2025-10-27 14:54:39 -0400

    fixed links

--- src/config.tsx
+++ src/config.tsx
@@ -9,21 +9,18 @@ export const otherLBAPIURL = "https://srv852039.hstgr.cloud/playergraphs/lb/"
 
 export const publicFileRoot = "https://dieplobbypicker.io" //""
 
-export const gameIDs: Record<string, number> = { // idk where these come from and they probably change every update
-  //"atl-fc83e7c455d1cbec.diep.io:2006": 63933,
-  "atl-fc83e7c455d1cbec.diep.io:2005": 4902,
-  "atl-fc83e7c455d1cbec.diep.io:2001": 28543,
-  //"atl-fc83e7c455d1cbec.diep.io:2007": 57903, // atl 2tdm 1
-  //"atl-fc83e7c455d1cbec.diep.io:2001": 33579, // atl 2tdm 2
-  "fra-43ce658e19c456d0.diep.io:2001": 25225, // fra 2tdm
-  "sgp-97b904129f583cef.diep.io:2001": 31220, // sgp 2tdm
-  "syd-c33a430721f75586.diep.io:2001": 43127, // syd 2tdm
+export const gameIDs: Record<string, number> = {
+  "atl-fc83e7c455d1cbec.diep.io:2001": 52867, // atl 2tdm
+  "atl-fc83e7c455d1cbec.diep.io:2003": 52867, // atl 4tdm
   
-  "atl-fc83e7c455d1cbec.diep.io:2006": 16177,
-  //"atl-fc83e7c455d1cbec.diep.io:2006": 11140, // atl 4tdm
-  "fra-43ce658e19c456d0.diep.io:2003": 25225, // fra 4tdm
-  "sgp-97b904129f583cef.diep.io:2003": 31220, // sgp 4tdm
-  "syd-c33a430721f75586.diep.io:2003": 43127, // syd 4tdm
+  "fra-43ce658e19c456d0.diep.io:2001": 65942, // fra 2tdm
+  "fra-43ce658e19c456d0.diep.io:2003": 52867, // fra 4tdm
+
+  "sgp-97b904129f583cef.diep.io:2001": 40904, // sgp 2tdm
+  "sgp-97b904129f583cef.diep.io:2003": 40904, // sgp 4tdm
+  
+  "syd-c33a430721f75586.diep.io:2001": 40904, // syd 2tdm
+  "syd-c33a430721f75586.diep.io:2003": 40904, // syd 4tdm
 }
 
 export const compensatePlayerCounts = true;
